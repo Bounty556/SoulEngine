2fe8cffb0adddc0ec4758c3130dcf8f6223b1641
Bounty556 <jacobmayday@gmail.com>
2020-12-16 14:51:29 -0700

    Added common typedefs

---
+++ SoulEngine/Source/UtilsLib/CommonTypes.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// File:			CommonTypes.h
+//
+// Author:			Peterson, Jacob
+//
+// Last Modified:	Date:	Dec 16 2020
+//					By:		Peterson, Jacob
+//					Exp:	Initial implementation
+//
+// Purpose:	        Contains commonly used typedefs for ease of reading
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+// Integer Types ///////////////////////////////////////////////////////////////
+typedef char Int8;
+typedef short int Int16;
+typedef int Int32;
+typedef long long int Int64;
+
+// Unsigned Integer Types //////////////////////////////////////////////////////
+typedef unsigned char Byte;
+typedef unsigned char UInt8;
+typedef unsigned short int UInt16;
+typedef unsigned int UInt32;
+typedef unsigned long long int UInt64;
+
+// Floating-Point Types ////////////////////////////////////////////////////////
+typedef float Float32;
+typedef double Float64;
