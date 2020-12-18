e739c9d0129d8af4e06a0bb6a984d404ea4587e1
Bounty556 <jacobmayday@gmail.com>
2020-12-17 18:41:15 -0700

    Finished minimal MemoryManager implementation using handles

--- SoulEngine/Source/UtilsLib/Memory/MemoryManager.cpp
+++ SoulEngine/Source/UtilsLib/Memory/MemoryManager.cpp
@@ -1,8 +1,116 @@
 /*
-Reserves an initial amount of memory for the engine to be used by allocators.
+Reserves an initial amount of memory for the engine to be allocated as needed.
 @file MemoryManager.cpp
 @author Jacob Peterson
-@version 2 12/17/20
+@version 3 12/17/20
 */
 
 #include "MemoryManager.h"
+
+#include <memory>
+
+#include <UtilsLib/Logger.h>
+
+namespace Soul
+{
+	Byte* MemoryManager::_suipMemoryStart;
+	Byte* MemoryManager::_suipAddressableMemoryStart;
+	Byte* MemoryManager::_suipMemoryEnd;
+	UInt32 MemoryManager::_uiHandleTableLength = 256;
+	Handle* MemoryManager::_sopHandleTableStart;
+	bool MemoryManager::_sbIsSetup = false;
+
+	void MemoryManager::StartUp(ByteCount uiByteSize)
+	{
+		Assert(!_sbIsSetup);
+
+		/*
+		Allocate the necessary memory
+		*/
+		_suiMemorySize = uiByteSize;
+		_suipMemoryStart = (Byte*)malloc(_suiMemorySize);
+		_suipMemoryEnd = _suipMemoryStart + _suiMemorySize;
+		_sopHandleTableStart = (Handle*)_suipMemoryStart;
+		_sopFirstHandle = nullptr;
+		_suipAddressableMemoryStart = (Byte*)(_sopHandleTableStart + _uiHandleTableLength);
+		_sbIsSetup = true;
+
+		memset(_sopHandleTableStart, 0, sizeof(Handle) * _uiHandleTableLength);
+	}
+
+	void MemoryManager::Shutdown()
+	{
+		Assert(_sbIsSetup);
+		free(_suipMemoryStart);
+	}
+
+	void MemoryManager::Defragment(UInt8 uiBlockCount)
+	{
+		// TODO: Implement
+	}
+
+	ByteCount MemoryManager::GetTotalAllocatedBytes()
+	{
+		Assert(_sbIsSetup);
+
+		ByteCount uiTotalBytes = 0;
+		Handle* opCurrentHandle = _sopFirstHandle;
+		while (opCurrentHandle)
+		{
+			uiTotalBytes += opCurrentHandle->uiByteSize;
+			opCurrentHandle = opCurrentHandle->opNextHandle;
+		}
+
+		return uiTotalBytes;
+	}
+
+	ByteCount MemoryManager::GetTotalFreeBytes()
+	{
+		Assert(_sbIsSetup);
+		return _suiMemorySize - GetTotalAllocatedBytes();
+	}
+
+	void* MemoryManager::FindFirstFreeMemoryBlock(ByteCount uiRequestedSize,
+		Handle** opPreviousHandleOut)
+	{
+		Handle* opCurrentHandle = _sopFirstHandle;
+		Handle* opNextHandle = opCurrentHandle->opNextHandle;
+
+		while (opNextHandle)
+		{
+			/*
+			Check the space between this handle and the next
+			*/
+			Byte* uipEndOfBlock =
+				(Byte*)opCurrentHandle->uipLocation + opCurrentHandle->uiByteSize;
+			if ((Byte*)opNextHandle->uipLocation - uipEndOfBlock >= uiRequestedSize)
+			{
+				(*opPreviousHandleOut) = opCurrentHandle;
+				return uipEndOfBlock;
+			}
+			else
+			{
+				opCurrentHandle = opNextHandle;
+				opNextHandle = opNextHandle->opNextHandle;
+			}
+		}
+
+		/*
+		There must be space between this last handle and the end of the
+		allocated memory, otherwise we have run out of memory and we will crash.
+		*/
+		Byte* uipEndOfBlock =
+			(Byte*)opCurrentHandle->uipLocation + opCurrentHandle->uiByteSize;
+		if (_suipMemoryEnd - uipEndOfBlock >= uiRequestedSize)
+		{
+			(*opPreviousHandleOut) = opCurrentHandle;
+			return uipEndOfBlock;
+		}
+		else
+		{
+			SoulLogError("Ran out of memory.");
+			Assert(false);
+			return nullptr;
+		}
+	}
+}
--- SoulEngine/Source/UtilsLib/Memory/MemoryManager.h
+++ SoulEngine/Source/UtilsLib/Memory/MemoryManager.h
@@ -1,5 +1,5 @@
 /*
-Reserves an initial amount of memory for the engine to be used by allocators.
+Reserves an initial amount of memory for the engine to be allocated as needed.
 @file MemoryManager.h
 @author Jacob Peterson
 @version 3 12/17/20
@@ -7,7 +7,7 @@ Reserves an initial amount of memory for the engine to be used by allocators.
 
 #pragma once
 
-#include <new>
+#include <utility>
 
 #include <UtilsLib/CommonTypes.h>
 #include <UtilsLib/Macros.h>
@@ -25,7 +25,7 @@ namespace Soul
 	struct Handle
 	{
 		Handle* opNextHandle; // The handle closest to this one.
-		Byte* uipLocation; // Location that this handle points to in the memory arena.
+		void* uipLocation; // Location that this handle points to in the memory arena.
 		ByteCount uiByteSize; // Size of the memory block that this handle points to.
 		UInt32 uiElementCount; // Number of elements allocated in the memory block.
 		bool bIsUsed; // Whether this handle is currently in use.
@@ -36,9 +36,10 @@ namespace Soul
 	initialized by calling StartUp() (usually done by the engine) and cleaned up
 	via Shutdown().
 
-	To partition memory for use, call Allocate<T>(...) with the type desired
-	inserted into the template. This will return a new UniqueHandle which can
-	then be used to access the memory block that was allocated for the object.
+	To partition memory for use, call Allocate<T>(...) or AllocateArray<T> with
+	the desired type inserted into the template. This will return a new
+	UniqueHandle which can then be used to access the memory block that was
+	allocated for the object.
 
 	For debugging purposes, the GetTotalAllocatedBytes() and GetTotalFreeBytes()
 	functions can be used to query the current usage of the memory arena. The
@@ -51,8 +52,10 @@ namespace Soul
 
 		/*
 		Initializes the MemoryManager's memory and sets up the Handle table.
+
+		@param uiByteSize - The number of bytes to reserve for this MemoryManager.
 		*/
-		static void StartUp();
+		static void StartUp(ByteCount uiByteSize);
 
 		/*
 		Shuts down the MemoryManager and frees all its memory.
@@ -60,19 +63,27 @@ namespace Soul
 		static void Shutdown();
 
 		/*
-		Attempts to allocate the provided amount of memory in the arena. The
-		total amount of memory reserved will be equal to uiBytes * uiCount.
+		Attempts to allocate memory for the provided object type.
 
-		@param uiBytes - The number of bytes needed to reserve one element of
-						 the provided type.
-
-		@param uiCount - The number of elements to reserve memory for.
+		@param oArgs - The arguments to initialize the object with.
 
 		@return UniqueHandle<T> containing the handle that points to the newly
 		                        allocated memory.
 		*/
+		template <class T, class... Args>
+		static UniqueHandle<T> Allocate(Args&&... oArgs);
+
+		/*
+		Attempts to allocate the provided amount of memory in the arena.
+
+		@param uiCount - The number of elements to reserve memory for in the
+		                 array.
+
+		@return UniqueHandle<T> containing the handle that points to the newly
+								allocated memory.
+		*/
 		template <class T>
-		static UniqueHandle<T> Allocate(ByteCount uiBytes, UInt32 uiCount);
+		static UniqueHandle<T> AllocateArray(UInt32 uiCount);
 
 		/*
 		Calls the destructor and frees the memory for every object allocated to
@@ -95,22 +106,111 @@ namespace Soul
 		Returns the total number of bytes that have been allocated by the
 		MemoryManager (this does not include the memory used by the Handle table)
 		*/
-		static void GetTotalAllocatedBytes();
+		static ByteCount GetTotalAllocatedBytes();
 
 		/*
 		Returns the total number of bytes that are free in the MemoryManager.
 		*/
-		static void GetTotalFreeBytes();
+		static ByteCount GetTotalFreeBytes();
 
 	private:
 		MemoryManager() = delete;
 
+		template <class T, class... Args>
+		static Handle* SetupNewHandle(UInt32 uiCount, Args&&... oArgs);
+
+		/*
+		Finds the first available memory block that can accomodate the
+		requested byte size.
+
+		@param uiRequestedSize - The number of bytes requested to be reserved.
+
+		@param opPreviousHandleOut - The handle just before the new block.
+
+		*/
+		static void* FindFirstFreeMemoryBlock(ByteCount uiRequestedSize,
+			Handle** opPreviousHandleOut);
+	
 	private:
 		static Byte* _suipMemoryStart; // Start of partitioned memory.
 		static Byte* _suipAddressableMemoryStart; // Start of memory that can be allocated to.
 		static Byte* _suipMemoryEnd; // End of addressable memory.
+		static ByteCount _suiMemorySize; // Size of total reserved memory.
 		static UInt32 _uiHandleTableLength; // Maximum amount of handles that can be created.
-		static Handle* _opHandleTableStart; // Start address of handle table.
-		static bool _bIsSetup; // Whether this MemoryManager has been initialized yet.
+		static Handle* _sopHandleTableStart; // Start address of handle table.
+		static Handle* _sopFirstHandle; // Address fo the starting handle of the table.
+		static bool _sbIsSetup; // Whether this MemoryManager has been initialized yet.
 	};
+
+	template <class T, class... Args>
+	UniqueHandle<T> MemoryManager::Allocate(Args&&... oArgs)
+	{
+		Assert(_sbIsSetup);
+
+		Handle* opNewHandle = SetupNewHandle<T>(1, std::forward<Args>(oArgs)...);
+		// TODO: 
+		// UniqueHandle<T> oUniqueHandle(*opNewHandle);
+		// return std::move(oUniqueHandle);
+	}
+
+	template <class T>
+	UniqueHandle<T> MemoryManager::AllocateArray(UInt32 uiCount)
+	{
+		Assert(_sbIsSetup);
+
+		Handle* opNewHandle = SetupNewHandle<T>(uiCount);
+		// TODO: 
+		// UniqueHandle<T> oUniqueHandle(*opNewHandle);
+		// return std::move(oUniqueHandle);
+	}
+
+	template <class T>
+	void MemoryManager::Deallocate(Handle& oHandle)
+	{
+		Assert(_sbIsSetup);
+
+		// TODO: Implement
+	}
+
+	template <class T, class... Args>
+	Handle* MemoryManager::SetupNewHandle(UInt32 uiCount, Args&&... oArgs)
+	{
+		// TODO: Move FindFirstFreeMemoryBlock call onto a separate thread.
+		/*
+		Find the first available memory slot that can accomodate this memory
+		block.
+		*/
+		Handle* opPreviousHandle = nullptr;
+		void* pAvailableBlock =
+			FindFirstFreeMemoryBlock(uiCount * sizeof(T), &opPreviousHandle);
+
+		/*
+		Find the first available Handle.
+		*/
+		Handle* opCurrentHandle = _sopHandleTableStart;
+		while (opCurrentHandle->bIsUsed)
+		{
+			++opCurrentHandle;
+		}
+
+		/*
+		Allocate memory and configure handles. We only need to construct the
+		object if we are not allocating for an array.
+		*/
+		if (uiCount == 1)
+		{
+			new (pAvailableBlock) T(std::forward<Args>(oArgs)...);
+		}
+		if (opPreviousHandle)
+		{
+			opCurrentHandle->opNextHandle = opPreviousHandle->opNextHandle;
+			opPreviousHandle->opNextHandle = opCurrentHandle;
+		}
+		opCurrentHandle->uipLocation = pAvailableBlock;
+		opCurrentHandle->uiByteSize = uiCount * sizeof(T);
+		opCurrentHandle->uiElementCount = uiCount;
+		opCurrentHandle->bIsUsed = true;
+
+		return opCurrentHandle;
+	}
 }
